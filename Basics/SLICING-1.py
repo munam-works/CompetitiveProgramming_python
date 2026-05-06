06f6139d0056852f586f7cba2e9df9b95ceba4f1
munam-works <moshabbabjamiltech@gmail.com>
2026-05-07 01:44:48 +0530

    Create SLICING-1.py

---
+++ Basics/SLICING-1.py
@@ -0,0 +1,12 @@
+first_string = 'Hello, World!'
+
+print(first_string)
+
+Len(first_string)
+
+first_string[1]
+
+first_string[1]='o'
+
+first_string[1:4]
+#immutable & Ordered:Are present in a ordered and cannot be changed
