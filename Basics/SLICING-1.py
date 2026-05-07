4f2aa57caf7e28baa478b475eb0a1025b48a47e0
munam-works <moshabbabjamiltech@gmail.com>
2026-05-07 14:37:34 +0530

    Update SLICING-1.py

--- Basics/SLICING-1.py
+++ Basics/SLICING-1.py
@@ -9,4 +9,26 @@ first_string[1]
 first_string[1]='o'
 
 first_string[1:4]
-#immutable & Ordered:Are present in a ordered and cannot be changed
+# immutable & Ordered:Are present in a ordered and cannot be changed
+
+string1 = 'Hello'
+
+string2 = 'World'
+
+string2 = str(20)
+
+string3 = string1 + ' ' + string2
+
+print(string3)
+
+first = 'munam'
+
+second = 'farhat'
+
+Timespent = '6 months'
+
+print(f'{first} and {second} have been together for {Timespent} Lovingly')
+
+newone= '{} & {} have been very happily together for {}' .format(first,second,Timespent)
+
+print(newone)
